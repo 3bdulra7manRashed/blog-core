8343b9b9cc450393362253e41df05edbb14c517e
3bdulra7manRashed <3bdulra7manra4ed@gmail.com>
2026-02-10 23:25:44 +0200

    feat: add CTA button partial for landing page views

--- modules/Landing/Resources/views/front/partials/cta.blade.php
+++ modules/Landing/Resources/views/front/partials/cta.blade.php
@@ -4,10 +4,5 @@
     <a href="{{ $cta['link'] }}" class="px-8 py-3 bg-white text-brand-primary font-bold rounded-full hover:bg-gray-100 transition-colors shadow-lg">
         {{ $cta['text'] }}
     </a>
-    @if(feature('contact') && Route::has('contact'))
-        <a href="{{ route('contact') }}" class="px-8 py-3 bg-transparent border-2 border-white text-white font-bold rounded-full hover:bg-white hover:text-brand-primary transition-all">
-            تواصل معي
-        </a>
-    @endif
 </div>
 @endif
